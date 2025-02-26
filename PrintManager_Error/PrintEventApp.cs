d0e08d8f1e304785a9ff710472ba622a79a2ab37
Timothy Logan <tlogan@hksinc.com>
2025-02-26 14:42:14 -0600

    Add project files.

---
+++ PrintManager_Error/PrintEventApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+
+namespace PrintManager_Error
+{
+    public class PrintEventApp : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            application.ControlledApplication.DocumentPrinting -= OnPrinting;
+            application.ControlledApplication.FileExporting -= OnExporting;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.ControlledApplication.DocumentPrinting += OnPrinting;
+            application.ControlledApplication.FileExporting += OnExporting;
+
+            return Result.Succeeded;
+        }
+
+        public void OnPrinting(object sender, DocumentPrintingEventArgs e)
+        {
+            List<ElementId> views = e.GetViewElementIds().ToList();
+            TaskDialog.Show("Printing", $"Printing {views.Count} Views based on e.GetViewElementIds()");
+
+            PrintManager printManager = e.Document.PrintManager;
+            ViewSet vs = printManager.ViewSheetSetting.CurrentViewSheetSet.Views;
+            TaskDialog.Show("Printing", $"Printing {vs.Size} Views based on printManager.ViewSheetSetting.CurrentViewSheetSet.Views");
+        }
+
+        public void OnExporting(object sender, FileExportingEventArgs e)
+        {
+            PrintManager printManager = e.Document.PrintManager;
+            ViewSet vs = printManager.ViewSheetSetting.CurrentViewSheetSet.Views;
+            TaskDialog.Show("Printing", $"Printing {vs.Size} Views based on printManager.ViewSheetSetting.CurrentViewSheetSet.Views");
+        }
+    }
+}
---
+++ PrintManager_Error/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrintManager_Error")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrintManager_Error")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("49509011-6e29-4ac3-a2e3-39e24847c16f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
